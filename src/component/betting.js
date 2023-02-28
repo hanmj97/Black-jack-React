8a14563a99c6f0ad15a76caddd26d2e31419d851
hanmj97 <hanmj97@naver.com>
2023-03-01 03:32:12 +0900

    Update perfect pair

--- src/component/betting.js
+++ src/component/betting.js
@@ -1,7 +1,7 @@
 import gametable from "../headerimg/gametable.jpg";
 import testcard from "../cardimg/1_ace_of_clubs.png";
 import testcard2 from "../cardimg/10_king_of_hearts.png";
-import nocard from "../cardimg/cardback.jpg";
+import nocard from "../cardimg/cardback.png";
 import chip1 from "../chipimg/1_white_chip.png";
 import chip5 from "../chipimg/5_red_chip.png";
 import chip10 from "../chipimg/10_blue_chip.png";
@@ -12,7 +12,11 @@ import Axios from "axios";
 import Swal from "sweetalert2";
 import { useNavigate } from 'react-router-dom';
 
+let userid = "";
 let usermoney = 0;
+
+
+userid = sessionStorage.getItem("id");
 
 Axios.post("http://localhost:8000/userinfo", {
     id: sessionStorage.getItem("id"),
@@ -24,6 +28,8 @@ Axios.post("http://localhost:8000/userinfo", {
   
 
 setInterval(() => {
+    userid = sessionStorage.getItem("id");
+
     Axios.post("http://localhost:8000/userinfo", {
         id: sessionStorage.getItem("id"),
     }).then((res) => {  
@@ -41,7 +47,7 @@ const Game = () => {
     const urlmove = useNavigate();
     const battingmoney = 0;
 
-    let [fade, setFade] = useState('')
+    let [fade, setFade] = useState('');
 
     useEffect(()=>{
         // tab의 상태가 변할때 (클릭 후 다른탭 열리면) 0.1초 뒤 'end' className 바인딩
@@ -51,7 +57,7 @@ const Game = () => {
             clearTimeout(fadeTimer);
   	        setFade('')
         }
-    }, [])
+    }, []);
 
     return (
         <div>
@@ -65,7 +71,6 @@ const Game = () => {
                     <img src={nocard} className="testcard2"/>
                 </div>
 
-
                 <div className="usercard1">
                     <img src={nocard} className="testcard"/>
                 </div>
@@ -75,13 +80,20 @@ const Game = () => {
 
                 <div className="usermoney">bankroll : {usermoney}</div>
 
-                <div className="chips">
+                <div className="betsfield">
+                    <a className="perfectbettext">Perfect Pair Bets : </a>
                     <a className="bettingtext">Bets : </a>
-                    <input type="number" className="bettingmoney" id="bettingmoney" disabled={true} value={battingmoney} ></input><a className="dallor">$</a>
+                    <input type="number" className="perfectbetmoney" id="perfectbetmoney" disabled={true} value={battingmoney} ></input><a className="perfectdallor">$</a>
+                    <input type="number" className="bettingmoney" id="bettingmoney" disabled={true} value={battingmoney} ></input><a className="betdallor">$</a>
+                </div>
+
+                <div className="chips">
                     <div className="betbtn1">
                         <button className="gbtn betreset" onClick={() => {
+                            const perfectElement = document.getElementById('perfectbetmoney');
                             const resultElement = document.getElementById('bettingmoney');
 
+                            perfectElement.value = 0;
                             resultElement.value = 0;
                         }}>Reset</button>
                     </div>
@@ -94,36 +106,55 @@ const Game = () => {
                         }}>All In</button>
                     </div>
                     <img src={chip5} className="chip5"     onClick={() => {
+                        const perfectElement = document.getElementById('perfectbetmoney');
                         const resultElement = document.getElementById('bettingmoney');
 
-                        if(Number(resultElement.value) + 5 <= usermoney){
+                        if(Number(perfectElement.value) + Number(resultElement.value) + 5 <= usermoney){
                             resultElement.value = Number(resultElement.value) + 5;
                         }
                     }}/>
                     <img src={chip10} className="chip10"   onClick={() => {
+                        const perfectElement = document.getElementById('perfectbetmoney');
                         const resultElement = document.getElementById('bettingmoney');
 
-                        if(Number(resultElement.value) + 10 <= usermoney){
+                        if(Number(perfectElement.value) + Number(resultElement.value) + 10 <= usermoney){
                             resultElement.value = Number(resultElement.value) + 10;
                         }
                     }}/>
                     <img src={chip25} className="chip25"   onClick={() => {
+                        const perfectElement = document.getElementById('perfectbetmoney');
                         const resultElement = document.getElementById('bettingmoney');
 
-                        if(Number(resultElement.value) + 25 <= usermoney){
+                        if(Number(perfectElement.value) + Number(resultElement.value) + 25 <= usermoney){
                             resultElement.value = Number(resultElement.value) + 25;
                         }
                     }}/>
                     <img src={chip100} className="chip100" onClick={() => {
+                        const perfectElement = document.getElementById('perfectbetmoney');
                         const resultElement = document.getElementById('bettingmoney');
 
-                        if(Number(resultElement.value) + 100 <= usermoney){
+                        if(Number(perfectElement.value) + Number(resultElement.value) + 100 <= usermoney){
                             resultElement.value = Number(resultElement.value) + 100;
                         }
                     }}/>
                 </div>
 
                 <div className="finishbats">
+                    <button className="gbtn perfectbets" onClick={() => {
+                        const perfectElement = document.getElementById('perfectbetmoney');
+                        const resultElement = document.getElementById('bettingmoney');
+
+                        if(Number(resultElement.value) > 50){
+                            Swal.fire({
+                                icon: "warning",
+                                title: "Perfect Pair",
+                                html: "베팅금액이 최대치를 벗어났습니다. <br/> (최대 베팅가능금액 : 50$)",
+                            });
+                        }else {
+                            perfectElement.value = resultElement.value;
+                            resultElement.value = 0;
+                        }
+                    }}>Perfect Pair Bets</button>
                     <button className="gbtn finbats" onClick={(e) =>  {
                         e.preventDefault();
 
@@ -165,7 +196,14 @@ const Game = () => {
                                         title: '베팅한 금액이 최소 베팅금액보다 적습니다.',
                                     });
                                 }else {
-                                    urlmove('/Game');
+                                    const resultElement = document.getElementById('bettingmoney');
+
+                                    urlmove('/Game', {
+                                        state: {
+                                            userid: userid,
+                                            betsmoney: Number(resultElement.value), 
+                                        }
+                                    });
                                 }
                             } else {
                                 const Toast = Swal.mixin({
--- src/component/game.js
+++ src/component/game.js
@@ -1,21 +1,25 @@
 import gametable from "../headerimg/gametable.jpg";
 import testcard from "../cardimg/1_ace_of_clubs.png";
 import testcard2 from "../cardimg/10_king_of_hearts.png";
-import nocard from "../cardimg/cardback.jpg";
+import nocard from "../cardimg/cardback.png";
 import React, { useEffect, useRef, useState } from 'react';
+import { useSpring, animated } from "react-spring";
 import Axios from "axios";
 import Swal from "sweetalert2";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Game = () => {  
     const urlmove = useNavigate();
+    const location = useLocation();
     const left = ["40%", "43%", "46%"];
     let usermoney = 0;
 
+    console.log(location.state);
+
     const randomcard = () => {
         Axios.post("http://localhost:8000/randomcard", {
-            
+            userid: location.state.userid,
+            betsmoney: location.state.betsmoney,
         }).then((res) => {
             if (res.data.affectedRows === 1){
                 const Toast = Swal.mixin({
@@ -50,17 +54,68 @@ const Game = () => {
         });
     }
 
+    let [fade, setFade] = useState('');
+
+    useEffect(()=>{
+        // tab의 상태가 변할때 (클릭 후 다른탭 열리면) 0.1초 뒤 'end' className 바인딩
+        const fadeTimer = setTimeout(()=>{ setFade('end') }, 100)
+        return ()=>{
+            // 기존 fadeTimer 제거 후 class 빈 값으로 변경
+            clearTimeout(fadeTimer);
+  	        setFade('')
+        }
+    }, []);
+
+    /*
+    const [flipped, setFlipped] = useState(false);
+
+    const { transform, opacity } = useSpring({
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 }
+    });
+    */
+
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = () => {
+      setIsFlipped(!isFlipped);
+    };
+
+
+    randomcard();
+
     return (
-        <div>
+        <div className={"start " + fade}>
             <div className="tablediv">
                 <img src={gametable} className='gametable' alt='gametable' />
 
-                <div className="delercard1">
-                    <img src={testcard} className="testcard"/>
+                <div className={`flip-container ${isFlipped ? 'flip' : ''}`} onClick={handleFlip}>
+                    <div className="flipper">
+                        <div className="front">
+                            <img src={testcard} alt="Front" style={{
+                                width: "10%",
+                                height: "20%",
+                            }}/>
+                        </div>
+                        <div className="back">
+                            <img src={nocard} alt="Back" style={{
+                                width: "10%",
+                                height: "20%",
+                            }}/>
+                        </div>
+                    </div>
                 </div>
-                <div className="delercard2">
-                    <img src={nocard} className="testcard2"/>
+                
+                {/* 
+                <div className="delercard1" onClick={() => setFlipped(!flipped)}>
+                    <animated.img src={testcard} className="testcard" style={{ opacity: opacity.interpolate(o => 1 - o), transform }}/>
+                    <animated.img src={nocard} className="testcard" style={{
+                        opacity,
+                        transform: transform.interpolate(t => `${t} rotateX(180deg)`)
+                    }}/>
                 </div>
+                 */}
 
                 <div >
                     {
@@ -72,9 +127,6 @@ const Game = () => {
                             )
                         })
                     }
-                    <div className="usercard2">
-                        <img src={testcard2} className="testcard2"/>
-                    </div>
                 </div>
 
 
