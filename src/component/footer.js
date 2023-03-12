c228d3afe74c85c71c7b9642bb929f2bb3986eed
hanmj97 <hanmj97@naver.com>
2023-03-12 22:18:32 +0900

    Update black_jack game ver1.3

--- src/component/footer.js
+++ src/component/footer.js
@@ -34,8 +34,8 @@ export default function Footer() {
                     <h3>Black-Jack Game</h3>
                     <p>This page is a blackjack game page that I made using REACT because I was bored. The source code on this page can be found in the Github link below.</p>
                     <ul className="socials">
-                        <li><a href="/"><FontAwesomeIcon icon={faGamepad} size="2x"/></a></li>
-                        <li><a href="/"><FontAwesomeIcon icon={faHouse} size="2x"/></a></li>
+                        <li><a href="/Black-jack-React"><FontAwesomeIcon icon={faGamepad} size="2x"/></a></li>
+                        <li><a href="/Black-jack-React"><FontAwesomeIcon icon={faHouse} size="2x"/></a></li>
                         <li><a href="https://blog.naver.com/hanmj97" target="_blank"><FontAwesomeIcon icon={faBlog} size="2x"/></a></li>
                         <li><a href="https://instagram.com/hmj_97_?igshid=Zjc2ZTc4Nzk=" target="_blank"><FontAwesomeIcon icon={faInstagramSquare} size="2x"/></a></li>
                         <li><a href="https://github.com/hanmj97/Black-jack-React" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x"/></a></li>
